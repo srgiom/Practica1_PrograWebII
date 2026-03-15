a87c054ac73f75d6e1c049ef9752f053701abfab
Sergio Moreno <113471709+srgiom@users.noreply.github.com>
2026-03-15 13:40:51 +0100

    Delete backend/node_modules directory

--- backend/node_modules/monaco-graphql/esm/graphql.worker.js
+++
@@ -1,6 +0,0 @@
-import { initialize } from 'monaco-editor/esm/vs/editor/editor.worker';
-import { GraphQLWorker } from './GraphQLWorker';
-globalThis.onmessage = () => {
-    initialize((ctx, createData) => new GraphQLWorker(ctx, createData));
-};
-//# sourceMappingURL=graphql.worker.js.map
